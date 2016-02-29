f8bf26baa16930f5748f15dd9d74d7a84e655670
Daniel Ku <kjunine@gmail.com>
2016-02-29 22:31:15 +0900

    Add more.

--- platforms/android/src/com/adobe/phonegap/push/YonhapnewsActivity.java
+++ platforms/android/src/com/adobe/phonegap/push/YonhapnewsActivity.java
@@ -1,12 +1,21 @@
 package com.adobe.phonegap.push;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class YonhapnewsActivity extends Activity implements PushConstants {
 
@@ -14,6 +23,48 @@ public class YonhapnewsActivity extends Activity implements PushConstants {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 3000);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String message = extras.getString("message");
+        boolean isActive = PushPlugin.isActive();
+        boolean isInForeground = PushPlugin.isInForeground();
+        Log.d(LOG_TAG, "isActive = " + isActive);
+        Log.d(LOG_TAG, "isInForeground = " + isInForeground);
+
+        String packageName = getPackageName();
+        Resources resources = getResources();
+        setContentView(resources.getIdentifier("yonhapnews_layout", "layout", packageName));
+
+        TextView content = (TextView) findViewById(resources.getIdentifier("yonhapnews_content", "id", packageName));
+        content.setText(message);
+
+        LinearLayout layout = (LinearLayout) findViewById(resources.getIdentifier("yonhapnews_layout", "id", packageName));
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.cancel();
+                processPushBundle();
+                finish();
+                reloadMainActivity();
+            }
+        });
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.y = 0; params.x = 0;
+        params.gravity = Gravity.TOP | Gravity.CENTER_HORIZONTAL;
+        getWindow().setAttributes(params);
+    }
+    protected void onCreate_Backup(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Intent intent = getIntent();
